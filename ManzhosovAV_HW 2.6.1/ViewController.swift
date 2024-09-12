c88d120521ab37ee8a5807752ab08bf5f9c95ec0
shenn0n <shenn0n4412@gmail.com>
2024-09-12 10:13:30 +0300

    Initial Commit

---
+++ ManzhosovAV_HW 2.6.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ManzhosovAV_HW 2.6.1
+//
+//  Created by Александр Манжосов on 12.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
